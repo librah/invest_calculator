f387d8199fb6aba0539046ec4a510a7e53118067
librah <huang.libra@gmail.com>
2018-06-18 23:55:56 +0800

    Remove history price download capability to simplify the code

--- symbol.py
+++ symbol.py
@@ -1,8 +1,8 @@
+import datetime
+import json
 import logging
 import os
-import requests
-import json
-import datetime
+
 import pandas as pd
 
 #
@@ -11,85 +11,80 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 cache_dir = os.path.join(os.path.dirname(__file__), 'cache')
-APIKEY = 'alphavantage_apikey'
 
 #
 # DateFrame columns
 #
-C_OPEN = '1. open'
-C_HIGH = '2. high'
-C_LOW = '3. low'
-C_CLOSE = '4. close'
-C_VOLUME = '5. volume'
+C_OPEN = 'open'
+C_HIGH = 'high'
+C_LOW = 'low'
+C_CLOSE = 'close'
+C_VOLUME = 'volume'
 
 
 class Symbol(object):
     def __init__(self, symb):
         self._symb = symb.lower()
-        self._cache_file = os.path.join(cache_dir, symb + '.json')
-        self._cache_file_mdate = None  # datetime.date instance when the file was updated
         self._data = None
+        for filename in (symb + '.json', symb + '.csv'):
+            filename = os.path.join(cache_dir, filename)
+            if os.path.isfile(filename):
+                self._load_history_data(filename)
+                break
+        if self._data is None:
+            logging.error('No symbol json/csv file found in %s for "%s"', cache_dir, symb)
+            raise Exception('No symbol json/csv file found')
 
-    def _download_history_price(self):
-        if os.getenv(APIKEY, '') == '':
-            logger.error('Missing required "alphavantage_apikey" env variable')
-            raise Exception('Missing required "alphavantage_apikey" env variable')
+    def _load_history_data(self, data_file):
+        file_ext = os.path.splitext(data_file)[1]
+        if file_ext == '.json':
+            with open(data_file) as f:
+                raw_data = json.load(f)
+            df = pd.DataFrame.from_dict(raw_data['Time Series (Daily)'], orient='index', dtype='float')
+            df = df.rename(index=str, columns={'1. open': 'open',
+                                               '2. high': 'high',
+                                               '3. low': 'low',
+                                               '4. close': 'close',
+                                               '5. volume': 'volume'})
+            df.index = pd.to_datetime(df.index)
+            df = df.sort_index(0)
+            self._data = df
+        elif file_ext == '.csv':
+            df = pd.read_csv(data_file, delim_whitespace=True, index_col=0, parse_dates=True)
+            df = df.rename(index=str, columns={'Open': 'open',
+                                               'High': 'high',
+                                               'Low': 'low',
+                                               'Close': 'close',
+                                               'Volume': 'volume'})
+            df.index = pd.to_datetime(df.index)
+            df = df.sort_index(0)
+            self._data = df
 
-        resp = requests.get('https://www.alphavantage.co/query', {
-            "function": "TIME_SERIES_DAILY",
-            "symbol": self._symb,
-            "apikey": os.getenv(APIKEY, ''),
-            "outputsize": "full"
-        })
-
-        if resp.status_code != 200:
-            logger.error('Unable to download "%s" history price from https://www.alphavantage.co\n%s', self._symb, resp.text)
-            raise Exception('Unable to download history price')
-
-        with open(self._cache_file, "w") as f:
-            json.dump(resp.json, indent=2)
-
-    def _load_price_data(self):
-        if not os.path.isfile(self._cache_file):
-            self._download_history_price()
-
-        with open(self._cache_file) as f:
-            raw_data = json.load(f)
-            self._cache_file_mdate = datetime.date.fromtimestamp(os.path.getmtime(self._cache_file))
-
-        df = pd.DataFrame.from_dict(raw_data['Time Series (Daily)'], orient='index', dtype='float')
-        df.index = pd.to_datetime(df.index)
-
-        self._data = df
-
-    def _can_refresh_cache(self):
-        # if the cache file was created today, no refesh needed
-        return datetime.date.today() != self._cache_file_mdate
-
-    def get_price(self, range):
+    def get_price(self, d_range):
         """
-        :param range: a date string, ex: '20180617', or date range tuple ('20180101', '20180617').
+        :param d_range: a date string, ex: '20180617', or date range tuple ('20180101', '20180617').
         :return: a tuple of price (open, high, low, close) in the specified time range.
           None returned if
         """
-        if self._data is None:
-            self._load_price_data()
-
-        if isinstance(range, basestring):
-            if range not in self._data.index:
+        if isinstance(d_range, basestring):
+            if d_range not in self._data.index:
                 return None
             else:
-                row = self._data.loc[range]
+                row = self._data.loc[d_range]
+                if isinstance(row, pd.DataFrame):
+                    row = row.iloc[0]
                 return (row[C_OPEN],
                         row[C_HIGH],
                         row[C_LOW],
                         row[C_CLOSE])
         else:
-            data_range = self._data.index.date
-            data_start_date = data_range[0]
-            data_end_date = data_range[-1]
+            data_date = self._data.index.date
+            data_start_date = data_date[0]
+            data_end_date = data_date[-1]
+            if data_start_date > data_end_date:
+                data_start_date, data_end_date = data_end_date, data_start_date
 
-            d1, d2 = range
+            d1, d2 = d_range
             query_start_date = datetime.datetime.strptime(d1, '%Y%m%d').date()
             query_end_date = datetime.datetime.strptime(d2, '%Y%m%d').date()
             assert query_end_date > query_start_date
--- tests/test_symbol.py
+++ tests/test_symbol.py
@@ -4,13 +4,14 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from symbol import Symbol
+import symbol
+
+symbol.cache_dir = os.path.dirname(__file__)
 
 
 class SymbolTest(unittest.TestCase):
-    def test_get_price(self):
-        symb_vt = Symbol('vt')
-        symb_vt._cache_file = os.path.join(os.path.dirname(__file__), 'vt.json')
+    def test_get_price_alphavantage_json(self):
+        symb_vt = symbol.Symbol('vt')
         self.assertEqual(symb_vt.get_price('2018-06-14'), (76.07, 76.1790, 75.87, 75.97))
         self.assertEqual(symb_vt.get_price('20180614'), (76.07, 76.1790, 75.87, 75.97))
         self.assertIsNone(symb_vt.get_price('20180630'))
@@ -19,6 +20,12 @@ class SymbolTest(unittest.TestCase):
         self.assertEqual(symb_vt.get_price(('20180402', '20180614')), (73.39, 76.2800, 71.60, 75.97))
         self.assertIsNone(symb_vt.get_price(('20180614', '20180630')))
 
+    def test_get_price_cnyes_csv(self):
+        symb_50 = symbol.Symbol('0050')
+        self.assertEqual(symb_50.get_price('20160726'), (69.0, 69.4, 68.85, 69.4))
+        self.assertEqual(symb_50.get_price('20180615'), (81.60, 81.95, 81.20, 81.95))
+        self.assertEqual(symb_50.get_price(('20160726', '20160801')), (69.0, 69.95, 68.85, 69.7))
+        self.assertEqual(symb_50.get_price(('20160726', '20180614')), (69.00, 83.10, 68.85, 81.75))
 
 if __name__ == '__main__':
     unittest.main()
